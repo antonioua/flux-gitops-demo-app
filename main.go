b2d50acf40bf93923a6301a107c1b65519235d3f
Anton Kuznietsov <xmananton@gmail.com>
2022-09-09 19:42:47 +0300

    add healthCheck

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	s := "Hello world"
 	fmt.Println(s)
+	healthFile, err := os.Create("/healthcheck")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = healthFile.Close(); err != nil {
+		panic(err)
+	}
+
 	time.Sleep(time.Minute * 5)
 }
